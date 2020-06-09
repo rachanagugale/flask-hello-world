ddc42e64edb39af3c4989eaead46b02d1bd5999d
Rachana Gugale <42697335+rachanagugale@users.noreply.github.com>
2020-06-09 18:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ def upload_video():
             video = request.files['myvideo']
 
             #video.save(os.path.join(app.config['VIDEO_UPLOADS']), video.filename)
-            path = os.getcwd()
-            print(path)
-            video.save(os.path.join(path, video.filename))
+            file_path = os.getcwd()
+            print(file_path)
+            video.save(os.path.join(file_path, video.filename))
+            full_filename = os.path.join(file_path, video.filename)
             print("Video saved")
             
-            print(os.listdir(path))
+            print(os.listdir(file_path))
 
             return redirect(request.url)
-    return render_template("upload_video.html")
+    return render_template("upload_video.html", user_image = full_filename)
 
 @app.route("/")
 def hello():
