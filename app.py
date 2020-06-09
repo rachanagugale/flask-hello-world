62c3c8b0b72b41307542de02c255a6098e16789a
Rachana Gugale <42697335+rachanagugale@users.noreply.github.com>
2020-06-09 17:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import render_template, request, redirect, jsonify
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.config['VIDEO_UPLOADS'] = "C:\\Users\\Rachana\\Desktop\\ProjectFrontend\\FlaskApp\\uploads"
+
+@app.route("/upload-video", methods=['GET', 'POST'])
+def upload_video():
+    if request.method == 'POST':
+        if request.files:
+            video = request.files['myvideo']
+
+            #video.save(os.path.join(app.config['VIDEO_UPLOADS']), video.filename)
+            video.save("C:\\Users\\Rachana\\Desktop\\ProjectFrontend\\FlaskApp\\uploads\\" + video.filename)
+            print("Image saved")
+
+            return redirect(request.url)
+    return render_template("upload_video.html")
+
+@app.route("/")
+def hello():
+    return render_template("upload_video.html")
